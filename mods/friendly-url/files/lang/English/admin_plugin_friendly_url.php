181d7c0892aba5a4a23252a080f36c89f82bcde2
daris <daris91@gmail.com>
2011-09-20 12:54:23 +0200

    deleting mods dir

--- mods/friendly-url/files/lang/English/admin_plugin_friendly_url.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$lang_admin_plugin_friendly_url = array(
-'Friendly URL scheme'			=> 'Friendly URL scheme',
-'URL scheme'					=> 'URL scheme',
-
-'Settings updated'				=> 'Settings updated',
-);
--- mods/most-users-online/files/lang/English/most_users_online.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$lang_most_users_online = array(
-'Most users online'			=> 'Most users ever online was %s on %s'
-);
